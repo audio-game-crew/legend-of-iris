0e93ddabeb941e88885b70d683928aaeaed49e4c
Mick van Gelderen <mickvangelderen@gmail.com>
2015-01-08 16:42:49 +0100

    Set tags for Beorne and Lucy

--- Assets/Scripts/Characters/Characters.cs
+++ Assets/Scripts/Characters/Characters.cs
@@ -4,15 +4,11 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class Characters
-{
+public class Characters {
     private static LucyController lucy;
-    public static LucyController Lucy
-    {
-        get 
-        {
-            if (lucy == null)
-                lucy = GameObject.FindGameObjectWithTag("Lucy").GetComponent<LucyController>();
+    public static LucyController Lucy {
+        get {
+            if (lucy == null) lucy = GameObject.FindGameObjectWithTag("Lucy").GetComponent<LucyController>();
             return lucy;
         }
     }
