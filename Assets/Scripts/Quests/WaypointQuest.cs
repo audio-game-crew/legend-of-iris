78c1b716aceb9e5193aca71e9009a2bee59b2129
Bas Dado <immortaly007@gmail.com>
2015-01-13 17:45:40 +0100

    Added Timeout and Wrong way conversation to waypointquest

--- Assets/Scripts/Quests/WaypointQuest.cs
+++ Assets/Scripts/Quests/WaypointQuest.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 public class WaypointQuest : Quest<WaypointQuest, WaypointQuestDefinition> {
 
 	public int index = 0;
+    private float timeoutTimer;
+    private float wrongWayTimer;
+    private float minTargetDistance;
+    private bool playingConversation = false;
+    private bool active = false;
 
-	public WaypointQuest(WaypointQuestDefinition definition) : base(definition) {}
+    public WaypointQuest(WaypointQuestDefinition definition) : base(definition) { }
 
     protected override void _Start()
     {
+        active = true;
 		base._Start();
 		Next();
 	}
 
 	private void Next() {
+        timeoutTimer = 0;
+        wrongWayTimer = 0;
 		if (index >= definition.waypoints.Count) {
 			Complete();
 			return;
@@ -22,6 +30,8 @@ public class WaypointQuest : Quest<WaypointQuest, WaypointQuestDefinition> {
 		waypoint.onPlayerEnter += OnPlayerEnter;
         var lucy = Characters.instance.Lucy.GetComponent<LucyController>();
         lucy.GotoLocation(new PositionRotation(waypoint.transform.position, waypoint.transform.rotation));
+        var player = Characters.instance.Beorn;
+        minTargetDistance = (player.transform.position - waypoint.transform.position).magnitude;
 	}
 
 	private void OnPlayerEnter(Waypoint waypoint, GameObject player) {
@@ -29,4 +39,38 @@ public class WaypointQuest : Quest<WaypointQuest, WaypointQuestDefinition> {
 		Next();
 	}
 
+    protected override void _Complete()
+    {
+        active = false;
+        base._Complete();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (!active)
+            return;
+        timeoutTimer += Time.deltaTime;
+        wrongWayTimer += Time.deltaTime;
+        var player = Characters.instance.Beorn;
+        var targetDistance = (player.transform.position - definition.waypoints[index].transform.position).magnitude;
+        if (targetDistance < minTargetDistance)
+            targetDistance = minTargetDistance;
+
+        if (definition.TimeoutActive && !playingConversation && timeoutTimer > definition.Timeout)
+            PlayConversation(definition.TimeoutConversationID);
+
+        if (definition.WrongWayActive && !playingConversation && wrongWayTimer > definition.WrongWayTimeout && targetDistance - minTargetDistance > definition.WrongWayThreshold)
+            PlayConversation(definition.WrongWayConversationID);
+    }
+
+    private void PlayConversation(string conversationID)
+    {
+        if (string.IsNullOrEmpty(conversationID))
+            return;
+        var player = ConversationManager.GetConversationPlayer(conversationID);
+        player.onConversationEnd += s => { timeoutTimer = 0; playingConversation = false; };
+        playingConversation = true;
+        player.Start();
+    }
 }
--- Assets/Scripts/Quests/WaypointQuestDefinition.cs
+++ Assets/Scripts/Quests/WaypointQuestDefinition.cs
@@ -4,9 +4,23 @@ using System.Collections.Generic;
 public class WaypointQuestDefinition : QuestDefinition {
 
 	public List<Waypoint> waypoints;
+    public string TimeoutConversationID = null;
+    public float Timeout = 10;
+    public string WrongWayConversationID = null;
+    public float WrongWayThreshold = 5;
+    public float WrongWayTimeout = 10;
 	
 	override public Quest Create() {
 		return new WaypointQuest(this);
 	}
+
+    public bool TimeoutActive
+    {
+        get { return !string.IsNullOrEmpty(TimeoutConversationID); }
+    }
 	
+    public bool WrongWayActive
+    {
+        get { return !string.IsNullOrEmpty(WrongWayConversationID); }
+    }
 }
