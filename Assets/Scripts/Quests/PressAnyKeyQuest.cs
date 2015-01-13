8cd6cd7ca3715302a81f2ecd1fcddca0818ef3c9
Mick van Gelderen <mickvangelderen@gmail.com>
2015-01-13 19:38:21 +0100

    Added scripts for PressAnyKey quest

---
+++ Assets/Scripts/Quests/PressAnyKeyQuest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PressAnyKeyQuest : Quest<PressAnyKeyQuest, PressAnyKeyQuestDefinition> {
+
+    private float lastConversationEnd = 0;
+    private ConversationPlayer player = null;
+
+    public PressAnyKeyQuest(PressAnyKeyQuestDefinition definition) : base(definition) { }
+
+    protected override void _Start() {
+        base._Start();
+        StartConversation();
+    }
+
+    public override void Update() {
+        if (Input.anyKeyDown) {
+            Complete();
+            return;
+        }
+        if (Time.time > lastConversationEnd + definition.repeatDelay)
+            StartConversation();
+    }
+
+    private void StartConversation() {
+        if (player != null) return;
+        player = ConversationManager.GetConversationPlayer(definition.conversationId);
+        player.onConversationEnd += OnConversationEnd;
+        player.Start();
+    }
+
+    private void OnConversationEnd(ConversationPlayer _player) {
+        _player.onConversationEnd -= OnConversationEnd;
+        player = null;
+        lastConversationEnd = Time.time;
+    }
+
+}
---
+++ Assets/Scripts/Quests/PressAnyKeyQuestDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PressAnyKeyQuestDefinition : QuestDefinition {
+	
+	public float repeatDelay = 10f;
+	public string conversationId = "T0.1";
+
+	override public Quest Create() {
+		return new PressAnyKeyQuest(this);
+	}
+
+}
