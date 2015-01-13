f33388c5d74e0673859997ec0449f60f3e90b772
Mick van Gelderen <mickvangelderen@gmail.com>
2015-01-13 23:22:45 +0100

    Added basics for first science bird

---
+++ Assets/Scripts/Quests/FirstScienceBirdQuest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScienceBirdQuest : Quest<FirstScienceBirdQuest, FirstScienceBirdQuestDefinition> {
+
+	/* This class needs to spawn a science bird somewhere around the player */
+	List<GameObject> prefabs = new List<GameObject>();
+
+	public FirstScienceBirdQuest(FirstScienceBirdQuestDefinition definition) : base(definition) {}
+
+    protected override void _Start() {
+		base._Start();
+		SpawnScienceBird();
+		SpawnCrapBird();
+		SpawnCrapBird();
+	}
+
+	private GameObject RandomSpawn(GameObject prefab) {
+		Vector3 spawnBase = Characters.instance.Beorn.transform.position;
+		Vector2 spawnOffset = UnityEngine.Random.insideUnitCircle * definition.spawnDistance;
+		Vector3 spawnLocation = spawnBase + new Vector3(spawnOffset.x, 0, spawnOffset.y);
+		return UnityEngine.Object.Instantiate(prefab, spawnLocation, Quaternion.identity) as GameObject;
+	}
+
+	private void SpawnScienceBird() {
+		var bird = RandomSpawn(definition.scienceBirdPrefab);
+		var waypoint = bird.GetComponent<Waypoint>();
+		waypoint.onPlayerEnter += OnPlayerEnterScienceBird;
+		prefabs.Add(bird);
+	}
+
+	private void SpawnCrapBird() {
+		var bird = RandomSpawn(definition.crapBirdPrefab);
+		var waypoint = bird.GetComponent<Waypoint>();
+		waypoint.onPlayerEnter += OnPlayerEnterCrapBird;
+		prefabs.Add(bird);
+	}
+
+	private void OnPlayerEnterScienceBird(Waypoint waypoint, GameObject player) {
+		waypoint.onPlayerEnter -= OnPlayerEnterScienceBird;
+		GameObject.Destroy(waypoint.gameObject);
+		Debug.Log("Yay you found the science bird", waypoint);
+	}
+
+	private void OnPlayerEnterCrapBird(Waypoint waypoint, GameObject player) {
+		waypoint.onPlayerEnter -= OnPlayerEnterCrapBird;
+		GameObject.Destroy(waypoint.gameObject);
+		Debug.Log("Crap you found a crap bird", waypoint);
+	}
+
+	public override void Update() {
+		base.Update();
+
+	}
+}
---
+++ Assets/Scripts/Quests/FirstScienceBirdQuestDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class FirstScienceBirdQuestDefinition : QuestDefinition {
+
+	public float spawnDistance = 10f;
+	public GameObject scienceBirdPrefab = null;
+	public GameObject crapBirdPrefab = null;
+
+	override public Quest Create() {
+		return new FirstScienceBirdQuest(this);
+	}
+	
+}
