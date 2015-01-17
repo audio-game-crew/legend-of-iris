d79f0384d1aeae760a43c88833b83727423a1a69
Bas Dado <immortaly007@gmail.com>
2015-01-17 07:14:07 +0100

    Working on Credits in the scene

---
+++ Assets/Scripts/Quests/ShowUIQuest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUIQuest : Quest<ShowUIQuest, ShowUIQuestDefinition> {
+    private ConversationPlayer player = null;
+
+    public ShowUIQuest(ShowUIQuestDefinition definition) : base(definition) { }
+
+    protected override void _Start() {
+        base._Start();
+        definition.EndScreen.SetActive(true);
+        if (!string.IsNullOrEmpty(definition.ConversationID))
+        {
+            player = ConversationManager.GetConversationPlayer(definition.ConversationID);
+            player.onConversationEnd += player_onConversationEnd;
+            player.Start();
+        }
+    }
+
+    void player_onConversationEnd(ConversationPlayer player)
+    {
+        this.Complete();
+    }
+
+    protected override void _Complete()
+    {
+        base._Complete();
+        definition.EndScreen.SetActive(false);
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Quests/ShowUIQuestDefinition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUIQuestDefinition : QuestDefinition
+{
+
+    public GameObject EndScreen;
+    public string ConversationID;
+
+	override public Quest Create() {
+        return new ShowUIQuest(this);
+	}
+
+}
