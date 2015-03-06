31a93392d4b0467f86988e6474d5dbf61c2474b7
Olivier Hokke <git@wolfox.nl>
2015-03-06 19:33:48 +0100

    Timer is now transparent and debug text is hidden. Both until mouse hovers over settings toggle icon, then they both become totally visible, until the mouse moves away again. Also made the time string a bit shorter to show only what is necessary.

--- Assets/Fader.cs
+++ Assets/Fader.cs
@@ -7,6 +7,10 @@ public class Fader : MonoBehaviour {
     CanvasGroup cg;
     public float speed = 1f;
     public bool display = true;
+    [Range(0f, 1f)]
+    public float minAlpha = 0f;
+    [Range(0f, 1f)]
+    public float maxAlpha = 1f;
 
     public void FadeOut()
     {
@@ -30,13 +34,13 @@ public class Fader : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        if (display && cg.alpha < 0.999999f)
+        if (display && cg.alpha < maxAlpha - 0.0001f)
         {
-            cg.alpha = Mathf.Clamp01(cg.alpha + speed * Time.deltaTime);
+            cg.alpha = Mathf.Clamp(cg.alpha + speed * Time.deltaTime, minAlpha, maxAlpha);
         }
-        else if (!display && cg.alpha > 0.0001f)
+        else if (!display && cg.alpha > minAlpha + 0.0001f)
         {
-            cg.alpha = Mathf.Clamp01(cg.alpha - speed * Time.deltaTime);
+            cg.alpha = Mathf.Clamp(cg.alpha - speed * Time.deltaTime, minAlpha, maxAlpha);
             if (cg.alpha < 0.0001f)
             {
                 gameObject.SetActive(false);
