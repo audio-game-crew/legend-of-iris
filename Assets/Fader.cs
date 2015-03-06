46c84efb765f20aad74ab0f1dd3f81223b4aaafe
Olivier Hokke <git@wolfox.nl>
2015-03-06 17:12:10 +0100

    Few story fixes such as door sound location, added the IRIS logo screen and its fade in and out

---
+++ Assets/Fader.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Fader : MonoBehaviour {
+
+    CanvasGroup cg;
+    public float speed = 1f;
+    public bool display = true;
+
+    public void FadeOut()
+    {
+        display = false;
+    }
+
+    public void FadeIn()
+    {
+        if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+        display = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+        cg = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (display && cg.alpha < 0.999999f)
+        {
+            cg.alpha = Mathf.Clamp01(cg.alpha + speed * Time.deltaTime);
+        }
+        else if (!display && cg.alpha > 0.0001f)
+        {
+            cg.alpha = Mathf.Clamp01(cg.alpha - speed * Time.deltaTime);
+            if (cg.alpha < 0.0001f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+	}
+}
---
+++ Assets/ShadowChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShadowChanger : MonoBehaviour {
+
+    Shadow s;
+    public float period;
+    public float magnitude;
+    public float start;
+
+    void Start()
+    {
+        s = GetComponent<Shadow>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector2 one = Vector2.one.flipy();
+        Vector2 shadow = one * start + one * Mathf.Sin(Time.time / period) * magnitude;
+        s.effectDistance = shadow;
+        GetComponent<RectTransform>().anchoredPosition = -shadow / 2f;
+	}
+}
