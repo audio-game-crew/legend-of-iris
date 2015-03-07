aae588631b67c0539757e2f3fc7e192fb86851cf
Olivier Hokke <git@wolfox.nl>
2015-03-07 01:10:08 +0100

    created the DEMO audio sources, which are supposed to contain just the instructions. For demo purposes. full story-related files will be skipped soon. Still working on it.

---
+++ Assets/DemoModeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoModeManager : MonoBehaviour {
+
+    public static DemoModeManager instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetDemonstrationMode()
+    {
+        demonstrationMode = true;
+    }
+
+    public void SetStoryMode()
+    {
+        demonstrationMode = false;
+    }
+
+    public bool demonstrationMode = false;
+}
