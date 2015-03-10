abd09b26b2b51e0a35717fc3c3c7fd9596d6e2d1
Olivier Hokke <git@wolfox.nl>
2015-03-10 23:05:16 +0100

    Created a script that makes screenshots by pressing F11

---
+++ Assets/Scripts/Utils/Tiny/Screenshotter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshotter : MonoBehaviour {
+
+    [Range(1, 5)]
+    public int screenshotScale = 1;
+    public string folder = "./";
+    public KeyCode button = KeyCode.F11;
+    public bool ctrl;
+    public bool alt;
+    public bool cmd;
+    public bool shift;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(button))
+        {
+            string filename = "Screenshot (" + Screen.width * screenshotScale + " x " + Screen.height * screenshotScale + ") " + System.DateTime.Now.ToString("yyyy-MM-dd hh.mm.ss") + ".jpg";
+            Application.CaptureScreenshot(folder + filename, screenshotScale);
+            SubtitlesManager.ShowSubtitle(3f, "Iris", "Screenshot saved at \n\n" + System.IO.Path.GetFullPath(folder + filename));
+        }
+	}
+}
